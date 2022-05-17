381c3a32f581ffe7ebbb83426887f6493ba83f15
Kuricyn Aleksey <al.curitsin@gmail.com>
2022-05-17 21:32:46 +0500

    Product model --add

--- Program.cs
+++ Program.cs
@@ -4,3 +4,20 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+public class Product
+{
+    public Product(string name, double price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public override string ToString()
+    {
+        return $"Товар: {this.Name}; цена: {this.Price}";
+    }
+
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
