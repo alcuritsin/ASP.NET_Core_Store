351ea7c92584df6b06b9a970a223b25c0eec0edc
Kuricyn Aleksey <al.curitsin@gmail.com>
2022-05-17 21:45:18 +0500

    Catalog --add

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
+List<Product> catalog = new List<Product>();
+CreateCatalog();
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+void CreateCatalog()
+{
+    //  Начальное заполнение каталога.
+    catalog.Clear();
+    
+    catalog.Add(new Product(name:"Продукт_01", price: 11.1));
+    catalog.Add(new Product(name:"Продукт_02", price: 22.2));
+    catalog.Add(new Product(name:"Продукт_03", price: 33.3));
+}
 
 public class Product
 {
